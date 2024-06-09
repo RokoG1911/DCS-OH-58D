537c49ab69e25549b0d3f3fdaf6e0e83f149109b
RokoG1911 <glavicroko@gmail.com>
2024-06-09 11:20:32 +0200

    Add files via upload
    124 A/7-17 CAV, 159th Combat Aviation Brigade, 2013-2014 deployment.

---
+++ 7-17 124/description.lua
@@ -0,0 +1,15 @@
+livery =
+{
+	{"SHR_TAILsection",	DIFFUSE		,	"body_tail_124", false},
+	{"Doors n mms",	DIFFUSE			,	"body_doors_mms_124", false},
+	{"Mainbody",	DIFFUSE			,	"body_front_main_124", false},
+	{"Body_jeff_retopo",	DIFFUSE		,	"58D_jacket_pilot_124", false},
+}
+
+custom_args =
+{
+	[446] = 1.0, -- 1.0 not visible "tailnumber"
+}
+
+name = "US Army 7-17 124"
+order = 1
