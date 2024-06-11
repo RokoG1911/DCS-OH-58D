e38850a963d54769128fd5d108425ab4dbf09e25
RokoG1911 <glavicroko@gmail.com>
2024-06-11 14:48:55 +0200

    Add files via upload
    537 F/3-17 CAV, 3rd Combat Aviation Brigade, 2009 deployment.

---
+++ F 3-17 537/description.lua
@@ -0,0 +1,17 @@
+livery =
+{
+	{"SHR_TAILsection",	DIFFUSE		,	"body_tail_537", false},
+	{"Doors n mms",	DIFFUSE			,	"body_doors_mms_537", false},
+	{"Mainbody",	DIFFUSE			,	"body_front_main_537", false},
+	{"ALQ144", 	DIFFUSE			,	"58D_alq144_537", false},
+	{"Body_jeff_retopo",	DIFFUSE		,	"58D_jacket_pilot_537", false},
+	{"shorts",    DIFFUSE            ,    "58D_short_pilot_01_537", false},
+}
+
+custom_args =
+{
+	[446] = 1.0, -- 1.0 not visible "tailnumber"
+}
+
+name = "US Army 3-17 F Troop Centaurs 537"
+order = 1
