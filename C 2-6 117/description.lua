8584e011fab13bd79b5323445429ca87542eca62
RokoG1911 <glavicroko@gmail.com>
2024-06-15 14:30:26 +0200

    Add files via upload
    117 C/2-6 CAV, 25th Combat Aviation Brigade, 2006 OIF deployment.

---
+++ C 2-6 117/description.lua
@@ -0,0 +1,17 @@
+livery =
+{
+	{"Mainbody",	DIFFUSE			,	"body_front_main_117", false},
+	{"Doors n mms",	DIFFUSE			,	"body_doors_mms_117", false},
+	{"SHR_TAILsection",	DIFFUSE		,	"body_tail_117", false},
+	{"Body_jeff_retopo",    DIFFUSE            ,    "58D_jacket_pilot_sand_01_sand", false},
+    	{"shorts",    DIFFUSE            ,    "58D_short_pilot_01_sand", false},
+	{"SHR_books_n_more",	DIFFUSE					,	"58d_books_n_co", false},
+}
+
+custom_args =
+{
+	[446] = 1.0, -- 1.0 not visible "tailnumber"
+}
+
+name = "US Army 2-6 C Troop Chaos 117"
+order = 1
