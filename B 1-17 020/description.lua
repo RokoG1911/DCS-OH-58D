099fa5c028906070d551e064e8b00b46322d957d
RokoG1911 <glavicroko@gmail.com>
2024-06-27 12:46:45 +0200

    Add files via upload
    975 C/1-17 CAV and 020 B/1-17 CAV, 82nd Combat Aviation Brigade, 2013 OEF Deployment.

---
+++ B 1-17 020/description.lua
@@ -0,0 +1,18 @@
+livery =
+{
+	{"Doors n mms",	DIFFUSE			,	"body_doors_mms_020", false},
+	{"Mainbody",	DIFFUSE			,	"body_front_main_020", false},
+	{"Body_jeff_retopo",    DIFFUSE         ,    "58D_jacket_pilot_01_975", false},
+	{"SHR_TAILsection",	DIFFUSE		,	"body_tail_020", false},
+	{"SHR_books_n_more",	DIFFUSE		,	"58d_books_n_co_020", false},
+	{"SHR_helmet_jamie",    DIFFUSE            ,    "58d_helmet_pilot_020", false};
+
+}
+
+custom_args =
+{
+	[446] = 1.0, -- 1.0 not visible "tailnumber"
+}
+
+name = "US Army 1-17 B Troop Bootleg 020"
+order = 1
---
+++ C 1-17 975/description.lua
@@ -0,0 +1,17 @@
+livery =
+{
+	{"Doors n mms",	DIFFUSE			,	"body_doors_mms_975", false},
+	{"Mainbody",	DIFFUSE			,	"body_front_main_975", false},
+	{"Body_jeff_retopo",    DIFFUSE         ,    "58D_jacket_pilot_01_975", false},
+	{"SHR_TAILsection",	DIFFUSE		,	"body_tail_975", false},
+	{"SHR_books_n_more",	DIFFUSE		,	"58d_books_n_co_975", false},
+	{"SHR_TAILsection",	DIFFUSE		,	"body_tail_975", false},
+}
+
+custom_args =
+{
+	[446] = 1.0, -- 1.0 not visible "tailnumber"
+}
+
+name = "US Army 1-17 C Troop Crusaders 975"
+order = 1
